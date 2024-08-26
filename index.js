42eee8fc7fa1686c86e43324a4abd0de28d62ac4
dhyocarriel <dhyovana.carriel@escola.pr.gov.br>
2024-08-26 22:57:06 +0000

    Variaveis 3F

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 let idade = 17;
-const anoNascimento = 2007;
-let anoAtual = 2024;
+const anoNascimento = 2007 ;
+let anoAtual = 2024 ;
 const nomeCompleto = "Dhyovana Carriel";
 
-console.log("Meu nome é " + nomeCompleto + " nascido no ano de " + anoNascimento + "e no ano atual " + anoAtual + "completo " + idade + " anos");
+console.log("Meu nome é " + nomeCompleto + " nascida no ano de " + anoNascimento + " e no ano atual " + anoAtual + " completo " + idade + " anos");
+
 anoAtual = anoAtual + 1
 console.log(anoAtual);
 
 idade = idade + 1;
 console.log(idade);
+
+
+//Agencia de viagens
+const ListaDeDestinos = new Array (
+  "Gramado",
+  "São Paulo",
+  "Porto Alegre",
+  "Brasilia",
+  "Salvador"
+);
+
+console.log(ListaDeDestinos);
+console.log(ListaDeDestinos[4]);
+ListaDeDestinos.push("Coronel Vivida");
+console.log (ListaDeDestinos);
+
